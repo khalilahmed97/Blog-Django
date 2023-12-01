9ac08ad7abea8c27f2094e1d7a9e8b435462f5a1
Khalil Ahmed Sharif <101020879+khalilahmed97@users.noreply.github.com>
2023-12-02 01:57:25 +0500

    Update urls.py

--- blog/blogs/urls.py
+++ blog/blogs/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
-from .views import BlogPostView, BlogPostCommentView, BlogPostCategory
+from .views import BlogPost,Comment,Category
 urlpatterns = [
     # POST REQUEST
-    path('post/new/', BlogPostView.as_view()),
-    path('category/new', BlogPostCategory.as_view()),
-    path('post/comment/', BlogPostCommentView.as_view()),
+    path('post/new/', BlogPost.as_view()),
+    path('category/new', Category.as_view()),
+    path('post/comment/', Comment.as_view()),
     
     # GET REQUEST
-    path('post/all/', BlogPostView.as_view()),
-    path('category/all/', BlogPostCategory.as_view()),
-    path('post/comment/all/', BlogPostCommentView.as_view()),
+    path('post/all/', BlogPost.as_view()),
+    path('category/all/', Category.as_view()),
+    path('post/comment/all/', Comment.as_view()),
 
+
+    path('post/<str:id>/', BlogPost.as_view()),
+    path('category/<str:id>/', Category.as_view()),
+
+    # COMMENTS (AUTHOR AND POST)
+    path('author/comment/<str:id>/', Comment.as_view()),
 ]
